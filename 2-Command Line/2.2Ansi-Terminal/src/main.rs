16ac2f469916fbea447cea9abfed811b45713c84
Brandon Piña <brandonpi265@gmail.com>
2022-06-18 11:17:50 -0500

    Module 2

---
+++ 2-Command Line/2.2Ansi-Terminal/src/main.rs
@@ -0,0 +1,19 @@
+use ansi_term::{Colour, Style};
+
+fn main() {
+    println!("This is {} in color, {} in color and {} in color",
+           Colour::Red.paint("red"),
+            Colour::Blue.paint("blue"),
+            Colour::Green.paint("green")
+        );
+
+        println!("{} and this is not",
+        Style::new().bold().paint("This is Bold"));
+
+
+        println!("{}, {} and {}",
+            Colour::Yellow.paint("This is colored"),
+            Style::new().bold().paint("this is bold"),
+            Colour::Yellow.bold().paint("This is bold and colored")
+    )
+}
